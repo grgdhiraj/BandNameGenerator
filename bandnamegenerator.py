5e87a170c221e9a7d0d0415d7c35f6642f0debd0
grgdhiraj <57953730+grgdhiraj@users.noreply.github.com>
2020-11-08 22:32:32 +0100

    Update bandnamegenerator.py

--- bandnamegenerator.py
+++ bandnamegenerator.py
@@ -1,6 +1,6 @@
 print ("Welcome to Band Generator")
 name = input ("What is your name?\n")
-hometown = input ("By the way, where did you grow up?\n")
+hometown = input ("Oh! By the way, where did you grow up?\n")
 genre = input ("What kind of music does your band play?\n")
 pet = input ("Lastly, tell me what is your pet animal's name? \n")
 print ("Well, your band name could be " + hometown + " " + pet + "!")
